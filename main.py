f0adad383ec413604a917c1001da19cb37f53fc0
evolvestin <evolvestin@gmail.com>
2022-09-27 02:54:34 +0300

    2.0.6 test printing

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def start(stamp):
     channel = os.environ['original']
     channel = int(channel) if channel.startswith('-100') else channel
     client = TelegramClient(os.environ['session'], int(os.environ['api_id']), os.environ['api_hash']).start()
+    print(client.get_entity(channel))
     with client:
         @client.on(events.NewMessage(chats=channel))
         async def channel_handler(event):
